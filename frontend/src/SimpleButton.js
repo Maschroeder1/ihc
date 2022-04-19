d186e97b314af65a3228c63cf95fa3518ee5f967
Maschroeder1 <mikaelarnez@hotmail.com>
2022-04-19 16:41:00 -0300

    more warning fixes

--- frontend/src/SimpleButton.js
+++ frontend/src/SimpleButton.js
@@ -24,7 +24,7 @@ export default class SimpleButton extends React.Component {
         return <button 
                 onMouseOver={event => event.target.style.background = strong_color} 
                 onMouseOut={event => event.target.style.background = color}
-                style={{ "background":color, "height": "25%", width: "100%", "margin-bottom": "2%" }} 
+                style={{ "background":color, "height": "25%", width: "100%", "marginBottom": "2%" }} 
                 onClick={() => this.click(value)}>
                 { value }
             </button>
@@ -44,7 +44,7 @@ export default class SimpleButton extends React.Component {
                 <div>
                     {this.isVisible() &&
                         <section style={{ height: '600px' }}>
-                            <div style={{ "text-align": "center", "margin-bottom": "1%" }}> {this.state.text} </div>
+                            <div style={{ "textAlign": "center", "marginBottom": "1%" }}> {this.state.text} </div>
                             { this.button("#b3ffc8", "#00ff46", 'YES') }
                             { this.button("#ff7a8c", "#ff0023", 'NO') }
                             { this.button("#efadff", "#cd00ff", 'DOES NOT MATTER') }
