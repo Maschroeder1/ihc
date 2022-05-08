f7bef071d5c5e8d183e7cea2a0baac6742fbc33f
Maschroeder1 <mikaelarnez@hotmail.com>
2022-05-07 23:04:28 -0300

    fix selected item bottom scroll problem

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -299,7 +299,7 @@ class App extends Component {
       <div>{buttons}</div>
       <ApiRequest apiRequest={this.state.apiRequest} traceFun={this.apiRequestFun} scrollAnchor={this.messagesEndRef} pageBottom={this.pageBottom} />
       <button style={{ "background": '#9fe5e1', "height": "40px", width: "100%", "marginBottom": "2%", "fontSize": "18px", 'marginTop': '20px' }} onClick={() => this.scrollToPlace()}>Voltar para cima</button>
-      <div key={'reff'} ref={this.props.pageBottom} />
+      <div key={'reff'} ref={this.pageBottom} />
       <button onClick={() => console.log(this.state.trace)}>Finalizar</button>
     </>)
   }
