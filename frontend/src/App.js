a0576b5f1d7c044675e9e7df4ed36d56035cac7a
Maschroeder1 <mikaelarnez@hotmail.com>
2022-05-08 00:26:26 -0300

    advanced tiered buttons

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -241,20 +241,142 @@ class App extends Component {
     </div>)
   }
 
+  singleToggle(filterName, innerFilterName) {
+    let apiRequestCopy = this.state.apiRequest
+    let currentFilter = apiRequestCopy[filterName]
+
+    if (currentFilter === undefined) {
+      currentFilter = [innerFilterName]
+    } else {
+      if (currentFilter.includes(innerFilterName)) {
+        currentFilter = currentFilter.filter(filter => filter !== innerFilterName)
+      } else {
+        currentFilter.push(innerFilterName)
+      }
+    }
+
+    if (currentFilter.length === 0) {
+      delete apiRequestCopy[filterName]
+    } else {
+      apiRequestCopy[filterName] = currentFilter
+    }
+
+    let newTrace = this.state.trace
+    newTrace.push({ "time": new Date() - this.state.initialTimestamp, "Filter": filterName })
+
+    this.setState({ apiRequest: apiRequestCopy, trace: newTrace })
+  }
+
+  linkedToggle(filterName, childrenKeys) {
+    let apiRequestCopy = this.state.apiRequest
+    let currentFilter = apiRequestCopy[filterName]
+
+    if (currentFilter === undefined) {
+      currentFilter = childrenKeys
+    } else {
+      let allChildrenSelected = childrenKeys.every(key => currentFilter.includes(key))
+
+      if (allChildrenSelected) {
+        currentFilter = currentFilter.filter(filter => !childrenKeys.includes(filter))
+      } else {
+        currentFilter = [...new Set(currentFilter.concat(childrenKeys))]
+      }
+    }
+
+    if (currentFilter.length === 0) {
+      delete apiRequestCopy[filterName]
+    } else {
+      apiRequestCopy[filterName] = currentFilter
+    }
+
+    let newTrace = this.state.trace
+    newTrace.push({ "time": new Date() - this.state.initialTimestamp, "Filter": filterName })
+
+    this.setState({ apiRequest: apiRequestCopy, trace: newTrace })
+  }
+
+  linkedButton(text, filterName, children) {
+    const STRONG_COLOR = "#fff500"
+    const LIGHT_COLOR = "#fffb96"
+
+    let isParentSelected = this.state.apiRequest[filterName] !== undefined && 
+      this.state.apiRequest[filterName].some(item => children.map(child => child.key).includes(item))
+    let parentColor = isParentSelected ? STRONG_COLOR : LIGHT_COLOR
+
+    let parent = (
+      <div style={{marginTop:'1rem', "height": "4rem", 'backgroundColor': '#919191', 'width': '250px'}}>
+        <button 
+          key="parent"
+          onMouseOver={event => event.target.style.background = STRONG_COLOR}
+          onMouseOut={event => event.target.style.background = parentColor}
+          style={{ "background": parentColor, "height": "2rem", width: "200px", 'marginLeft': '20px', "marginTop": "1rem", "fontSize": "15px" }}
+          onClick={() => this.linkedToggle(filterName, children.map(child => child.key))}
+          >
+        {text}
+      </button>
+    </div>)
+    
+    let childrenButtons = []
+
+    for (let child of children) {
+      let isSelected = this.state.apiRequest[filterName] !== undefined && this.state.apiRequest[filterName].includes(child.key)
+      let color = isSelected ? STRONG_COLOR : LIGHT_COLOR
+
+      childrenButtons.push(
+        <div style={{marginTop:'1rem', "height": "4rem", 'backgroundColor': '#d6d6d6', 'width': '250px'}}>
+          <button
+          key={child.key}
+          onMouseOver={event => event.target.style.background = STRONG_COLOR}
+          onMouseOut={event => event.target.style.background = color}
+          style={{ "background": color, "height": "2rem", width: "160px", 'marginLeft': '10px', "marginTop": "1rem", "fontSize": "15px" }}
+          onClick={() => this.singleToggle(filterName, child.key)}
+          >
+            {child.text}
+          </button>
+        </div>
+      )
+    }
+
+    return(<div style={{display: "flex", flexDirection: "row"}}>
+    {parent}
+    {childrenButtons}
+    </div>)
+  }
+
   complexView() {
     return (
       <>
+      <div style={{ 'display': 'grid', 'gridTemplateColumns': '14rem 14rem 14rem 14rem 14rem', 'gridGap': '2rem', 'gridAutoFlow': 'row', 'columnGap': '10rem' }}>
+        {this.fillableInputRange("Faixa de preço do aluguel", 'TOTAL_PRICE_RANGE', 1000)} 
+        {this.fillableInputRange("Outras taxas recorrentes", 'FEES_PRICE_RANGE', 200)} 
+        {this.fillableInputRange("Número de quartos", 'BEDROOM COUNT', 0)}
+        {this.fillableInputRange("Número de banheiros", 'BATHROOM_COUNT', 0)}
+        {this.fillableInputRange("Número de vagas", 'PARKING_SPOTS', 0)}
+      </div>
         <div style={{ 'display': 'grid', 'gridTemplateColumns': '4rem 4rem 4rem 4rem', 'gridGap': '4rem', 'gridAutoFlow': 'row', 'columnGap': '20rem' }}>
           {this.boolButton("Pet friendly?", 'PET_FRIENDLY')}
           {this.boolButton("Mobiliado?", 'READY_TO_LIVE')}
           {this.boolButton("Com churrasqueira?", 'BARBECUE')}
           {this.boolButton("Com piscina?", 'POOL')}
         </div>
-        <div style={{ 'display': 'grid', 'gridTemplateColumns': '14rem 14rem 14rem 14rem', 'gridGap': '2rem', 'gridAutoFlow': 'row', 'columnGap': '10rem' }}>
-          {this.fillableInputRange("Faixa de preço do aluguel", 'TOTAL_PRICE_RANGE', 1000)} 
-          {this.fillableInputRange("Número de quartos", 'BEDROOM COUNT', 0)}
-          {this.fillableInputRange("Número de banheiros", 'BATHROOM_COUNT', 0)}
-          {this.fillableInputRange("Número de vagas", 'PARKING_SPOTS', 0)}
+        <div>
+          { this.linkedButton('Apartamentos', 'HOUSE_TYPE', [
+            {'text': 'Tradicional', 'key': 'TRADITIONAL'}, 
+            {'text': 'JK/Studio', 'key': 'STUDIO'}, 
+            {'text': 'Kitnet', 'key': 'KITNET'}, 
+            {'text': 'Flat', 'key': 'FLAT'}, 
+            {'text': 'Loft', 'key': 'LOFT'}, 
+            {'text': 'Andar', 'key': 'FLOOR'}, 
+            {'text': 'Cobertura', 'key': 'ROOFTOP'}
+            ])}
+        </div>
+        <div>
+            { this.linkedButton('Casa', 'HOUSE_TYPE', [
+              {'text': 'Casa residencial', 'key': 'SINGLE_FAMILY'}, 
+              {'text': 'Duplex', 'key': 'DUPLEX'}, 
+              {'text': 'Sítio', 'key': 'CABIN'}, 
+              {'text': 'Casa em condomínio', 'key': 'HOA'}
+              ])}
         </div>
       </>
     )
